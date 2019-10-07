a8c8adb58eb90a3b600a632f5243244a2db18fe2
Alex Samets <aliaksandr.samets@gmail.com>
2019-10-07 04:31:55 +0300

    Modified service. Gem faraday added

--- lib/services/requests/equation.rb
+++ lib/services/requests/equation.rb
@@ -3,11 +3,20 @@ module Services
     class Equation
       class << self
         def call(params)
+          request(params)
         end
 
         private
 
-        def request
+        def request(params)
+          url = 'https://shrouded-mountain-95416.herokuapp.com/equations'
+          resp = Faraday.post(url, equation_params(params),
+          "Content-Type" => "application/json")
+          resp.body
+        end
+
+        def equation_params(params)
+          {"a": params["a"].to_f, "b": params["b"].to_f, "c": params["c"].to_f, "type": params["type"]}.to_json
         end
       end
     end
