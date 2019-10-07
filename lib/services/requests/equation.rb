2ddabd7090bbd6f4a97ebbb1f5ac100bcc8485e9
Alex Samets <aliaksandr.samets@gmail.com>
2019-10-07 04:29:57 +0300

    Created euqation service

---
+++ lib/services/requests/equation.rb
@@ -0,0 +1,15 @@
+module Services
+  module Requests
+    class Equation
+      class << self
+        def call(params)
+        end
+
+        private
+
+        def request
+        end
+      end
+    end
+  end
+end
